c6a0d796b3e178b37a16014c7c659f7edb246e53
Dave Whitehouse <dave@jsdi.co.uk>
2015-08-23 14:46:02 +0100

    Getting valid response back now to POST request for login

--- apiTest.py
+++ apiTest.py
@@ -12,46 +12,33 @@ def login_api(username,password,mclient,url):
         session_key = mc.get("sessionkey")
 
         if session_key:
-		logging.info("Found session key in memcache")
-                return session_key
+            logging.info("Found session key in memcache")
+            return session_key
         else:
-		# prepare body json for login key request
-		logging.info("No session key in memcache, fetching a new one")
+            encpass = md5pass(password)
+            logging.info("No session key in memcache, fetching a new one")
+            rest_data = {'user_auth' : {'user_name' : username, 'password' : encpass}}
+            logging.info("JSON data" + json.dumps(rest_data))
+            post_args = {'method': 'login', 'input_type' : 'json', 
+            'response_type' : 'json', 'rest_data' : json.dumps(rest_data) }
+            
+            response_data = requests.post(url, verify=False, data=post_args)
+            reponse_code = response_data.status_code
+            if reponse_code != 200:
+                logging.error("session key request failed")
+                response_data.raise_for_status()
+                raise http_error
+            string = response_data.text
+            print "Response " + string
+            
+        return session_key
 
-		uname = { 'user_name' : username }
-		upass = { 'password' : password }
-		auth_creds = []
-		auth_creds.append(uname)
-		auth_creds.append(upass)
 
-		name_val = []
-		lang = { "language" : "en_uk" }
-		notify =  { "notifyonsave" : True }
-		name_val.append(lang)
-		name_val.append(notify)
 
-		user_auth = {}
-		user_auth["name_value_list"] = name_val
-		user_auth["application_name"] = "DaveTest"
-		user_auth["user_auth"] = auth_creds
-
-		print "JSON " + json.dumps(user_auth)
-		post_args = {'method': 'login', 'input_type' : 'JSON', 'response_type' : 'JSON', 'rest_data' : json.dumps(user_auth) }
-
-		response_data = requests.post(url, data=post_args)
-		reponse_code = response_data.status_code
-		if reponse_code != 200:
-                	logging.error("session key request failed")
-			response_data.raise_for_status()
-			raise http_error
-
-		string = response_data.text
-		print "Response " + string
-
-#		logging.info("adding new session key to memcache " + str(session_key) + " with ttl " + str(ttl) )
-#               mc.set("sessionkey", session_key, int(ttl))
-                return session_key
-
+def md5pass(password):
+    encoded = hashlib.md5(password.encode('utf-8'))
+    mypass = encoded.hexdigest()
+    return mypass
 
 
 #Get Global config attributes
@@ -61,8 +48,6 @@ try:
 	so_url = config["signon_url"]
 	username = config["username"]
 	password = config["password"]
-	md5bin = hashlib.md5(password)
-	md5pass = md5bin.hexdigest()
 	mc = config["memcache_client"]
 	logfile = config["logfile"]
 	logging.basicConfig(filename=logfile,level=logging.DEBUG)
@@ -71,4 +56,4 @@ except:
 	raise
 	
 
-session_key = login_api(username,md5pass,mc,so_url)
+session_key = login_api(username,password,mc,so_url)
